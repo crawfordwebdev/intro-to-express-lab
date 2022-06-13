b3ca1a489a742d6b6c176fcaaf187480e69dc25d
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-06-13 15:02:18 -0600

    adds server.js boilerplate

---
+++ server.js
@@ -0,0 +1,30 @@
+// import modules
+import express from 'express'
+
+// Create Express app
+const app = express()
+
+// Configure the app (app.set)
+app.set('view engine', 'ejs')
+
+// Mount Middleware (app.use)
+
+
+// Mount routes
+app.get('/', function(req, res) {
+  res.render('index')
+})
+
+// No middleware functions or routes found
+// https://expressjs.com/en/starter/faq.html
+app.use((req, res, next) => {
+  res.status(404).send("404: Sorry can't find that!")
+})
+
+
+
+// Tell the app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000')
+})
